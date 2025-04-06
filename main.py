afb0502e4605ebdfae48bbef3d9cb9c1d55f0d41
jmrkd-studio <108296592+MaheshTheDeveloper@users.noreply.github.com>
2025-04-06 08:34:05 +0530

    --update

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hi, This project is still under development, we will soon be releasing the first versions.")
+from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PROMPT = input(str("Prompt: "))
+
+google_client = genai.Client(api_key=os.getenv('GEMINI_API_KEY'))
+
+response = google_client.models.generate_content(
+    model="gemini-2.0-flash", contents=PROMPT
+)
+
+print(response.text)
